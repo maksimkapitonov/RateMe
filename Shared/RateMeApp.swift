1e6ffe38f3bbcc4890c95148c1d898532b59cfc0
Maksim Kapitonov <maksim@startted.com>
2021-02-15 13:49:53 +0300

    Initial Commit

---
+++ Shared/RateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMeApp.swift
+//  Shared
+//
+//  Created by Maksim Kapitonov on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct RateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
